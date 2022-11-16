63c49330d10e3c477856a0e7b4d0ce4ed6119830
Dima Zaporozhets <p1ink.gg@mail.ru>
2022-11-16 13:37:58 +0300

    first commit

---
+++ app/src/main/java/com/example/linesandshapes/PlayActivity.kt
@@ -0,0 +1,17 @@
+package com.example.linesandshapes
+
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+import android.graphics.*
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+
+    }
+}
